8bceaad83035cd29995a4120f130801af1b414b3
dev-lkj <2_kj@daum.net>
2024-01-21 16:25:28 +0900

    기본형과 참조형 추가

---
+++ src/ref/MethodChange1.java
@@ -0,0 +1,16 @@
+package ref;
+
+public class MethodChange1 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("a = " + a);
+        System.out.println("메서드 호출 전: a = " + a);
+        changePrimitive(a);
+        System.out.println("메서드 호출 후: a = " + a);
+    }
+
+    private static void changePrimitive(int x) {
+        System.out.println("x = " + x);
+        x = 20;
+    }
+}
