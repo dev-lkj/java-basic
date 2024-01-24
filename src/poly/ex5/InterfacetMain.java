bdc0c87fbdbaad8d0a2e274f12eb330f495ce513
dev-lkj <2_kj@daum.net>
2024-01-24 18:24:22 +0900

    다형성 추가

---
+++ src/poly/ex5/InterfacetMain.java
@@ -0,0 +1,38 @@
+package poly.ex5;
+
+
+public class InterfacetMain {
+    public static void main(String[] args) {
+        // 인터페이스 생성불가
+//        InterfaceAnimal interfaceMain1 = new InterfaceAnimal();
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+
+
+        soundAnimal(dog);
+        soundAnimal(cat);
+        soundAnimal(caw);
+
+        moveAnimal(dog);
+        moveAnimal(cat);
+        moveAnimal(caw);
+
+    }
+
+    //변하지 않는 부분
+    private static void soundAnimal(InterfaceAnimal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+
+    private static void moveAnimal(InterfaceAnimal animal) {
+        System.out.println("동물 이동 테스트 시작");
+        animal.move();
+        System.out.println("동물 이동 테스트 종료");
+    }
+
+
+}
